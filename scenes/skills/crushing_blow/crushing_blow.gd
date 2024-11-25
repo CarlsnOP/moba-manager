acdcd3044a0bbc260895880acc75ec37596b015e
CarlsnOP <Anderscarlsen94@gmail.com>
2024-11-25 11:23:36 +0200

    Implemented Mystic overflow
    every 20 seconds Todd deals additional dmg equal to his ability power

--- scenes/skills/crushing_blow/crushing_blow.gd
+++ scenes/skills/crushing_blow/crushing_blow.gd
@@ -2,7 +2,9 @@ extends Node
 
 var damage_mulitplier := 1.20
 
-# Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	var parent = get_parent()
 	parent._damage = parent._damage * damage_mulitplier
+
+
+	
--- scenes/skills/mystic_overflow/mystic_overflow.gd
+++ scenes/skills/mystic_overflow/mystic_overflow.gd
@@ -1,1 +1,29 @@
 extends Node
+
+@onready var ability_timer: Timer = %AbilityTimer
+
+var parent: Node2D
+var ability_ready := true
+var ability_cooldown := 20.0
+
+func _ready() -> void:
+	ability_timer.wait_time = ability_cooldown
+	parent = get_parent()
+	
+	if parent:
+		var timer = parent.get_node("AttTimer")
+
+		if timer and timer is Timer:
+			timer.connect("timeout", Callable(self, "_on_att_timer_timeout"))
+
+func _on_att_timer_timeout() -> void:
+	if parent._target != null and ability_ready:
+		magic_attack()
+		
+func magic_attack() -> void:
+	parent._target.take_damage(parent._ability_power, parent)
+	ability_ready = false
+	ability_timer.start()
+
+func _on_ability_timer_timeout() -> void:
+	ability_ready = true
