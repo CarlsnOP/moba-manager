a446d7daa94871b8f2e43e4ce5a5f6bdee8068c3
CarlsnOP <Anderscarlsen94@gmail.com>
2025-01-19 15:20:29 +0200

    Add files via upload

---
+++ resources/data_storage.gd
@@ -0,0 +1,27 @@
+class_name DataStorage
+extends Resource
+
+
+#PNG FILES
+#Status effects
+const IRONCLAD_AURA = preload("res://assets/art/map/status_effects/ironclad_aura.png")
+const SLOWED = preload("res://assets/art/map/status_effects/slowed.png")
+const STUNNED = preload("res://assets/art/map/status_effects/stunned.png")
+
+#Skills
+const IRONCLAD_AURA_RANGE = preload("res://assets/art/skills/extra/ironclad_aura_range.png")
+const CRUSHING_BLOW_PARTICLES = preload("res://battlesim/effects/crushing_blow_particles/crushing_blow_particles.tscn")
+const MYSTIC_OVERFLOW_PROJECTILE = preload("res://assets/art/skills/extra/mystic_overflow_projectile.png")
+const SECOND_CHANCE_PARTICLES = preload("res://battlesim/effects/second_chance_particles/second_chance_particles.tscn")
+
+#Particles
+const BASE_EXPLOSION = preload("res://battlesim/effects/explosion/base_explosion.tscn")
+const HEALING_PARTICLES = preload("res://battlesim/effects/healing/healing_particles.tscn")
+const DEATH_PARTICLES = preload("res://battlesim/effects/death_particles/death_particles.tscn")
+
+#MISC
+const BASE_PROJECTILE = preload("res://battlesim/projectiles/base_projectile/base_projectile.tscn")
+
+#UI
+const ACHIEVED_DUCK = preload("res://assets/art/achievements/achieved_duck.png")
+const UNACHIEVED_DUCK = preload("res://assets/art/achievements/unachieved_duck.png")
